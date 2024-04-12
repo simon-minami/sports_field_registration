d7945097d144d13e2a0d9e7c0d3ee714ce1ebd1d
simon minami <simon.sho.minami@gmail.com>
2024-04-12 17:06:57 -0400

    added inference file

---
+++ inference.py
@@ -0,0 +1,46 @@
+'''
+
+given input image, gets the predicted homography
+RIGHT NOW: trying to implement on the swim model, swim image etc.
+AFTER: once we confirms it works on the swim stuff, we can try training basketball model and then doing inference
+'''
+import torch
+from torch import load, unsqueeze, stack, no_grad
+from torchvision import transforms
+from torchvision.transforms.functional import rotate as rotate_tensor
+
+import os
+
+import numpy as np
+
+
+from model import deeper_Unet_like, vanilla_Unet
+from model_deconv import vanilla_Unet2
+
+from utils.blobs_utils import get_boxes, a_link_to_the_past, get_local_maxima
+from video_display_dataloader import get_video_dataloaders
+from utils.grid_utils import get_landmarks_positions, get_faster_landmarks_positions,\
+     get_homography_from_points, conflicts_managements, display_on_image
+
+
+
+if __name__=='__main__':
+    field_length = 50
+    markers_x = np.linspace(0, field_length, 11)
+    field_width = 25
+    lines_y = np.linspace(0, field_width, 11)
+
+    path = 'pool model.pth'
+    model = vanilla_Unet2(final_depth=len(markers_x) + len(lines_y))
+
+    batch_size = 64
+
+    models_path = 'model'
+
+    model_path = os.path.join(models_path, path)
+    model.load_state_dict(load(model_path))
+    model = model.cuda()
+    model.eval()
+    print('loaded model')
+    # with no_grad():
+    #     pass
