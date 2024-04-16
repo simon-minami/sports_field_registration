73a547c92fc1536499cdeebc6a275edb01bba3f4
simon minami <simon.sho.minami@gmail.com>
2024-04-16 02:42:50 -0400

    inference

--- inference.py
+++ inference.py
@@ -112,9 +112,7 @@ if __name__ == '__main__':
                                        field_length=field_length, field_width=field_width)
         warped_img = cv2.warpPerspective(img, H.astype(float), size)
 
-        H_temp, _ = cv2.findHomography(np.array(src_pts), np.array(dst_pts), cv2.RANSAC)
-        H_court_to_video = np.linalg.inv(H_temp)
-        H_court_to_video = H_court_to_video.astype(float)
+
         draw_img = copy.copy(img)
         # # testing drawing outline
         # # NOTE: drawing the original video key points on original image
@@ -180,74 +178,55 @@ if __name__ == '__main__':
 
 
 
-        # # # NOTE: drawing the original video key points on warped iamge
-        # video_pts = np.array(src_pts).reshape(-1, 1, 2).astype(float)  # need to reshape for transformation
-        # video_pts_transformed = cv2.perspectiveTransform(video_pts, H.astype(float))
-        # video_pts_transformed = video_pts_transformed.astype(int).reshape(-1, 2)
-        # print(f'debug: {video_pts_transformed.shape}, {video_pts_transformed}')
-        # for pt in video_pts_transformed:
-        #     cv2.circle(warped_img, pt, 3, (0, 0, 255), -1)
-        #
         # # NOTE: trying to draw court lines!!!!
-        # court_pts = np.array(
-        #     [(75, 19), (75, 31)]
-        # ).reshape(-1, 1, 2).astype(float)
-        # court_pts_transformed = cv2.perspectiveTransform(court_pts, H_court_to_video)
-        # court_pts_transformed = court_pts_transformed.astype(int).reshape(-1, 2)
-        # print(f'debug: {video_pts_transformed.shape}, {video_pts_transformed}')
-        # names = ['rk top left', 'rk bottom left']
-        # for pt, name in zip(video_pts_transformed, names):
-        #     cv2.circle(draw_img, pt, 3, (0, 0, 255), -1)
-        #     cv2.putText(draw_img, name, (pt[0] - 30, pt[1] - 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+        court_corners = np.array([
+            [0, 0], [94, 0], [94, 50], [0, 50]
+        ], dtype=float)
+        right_key = np.array([
+            (75, 19), (94, 19), (94, 31), (75, 31)
+        ], dtype=float)
+        half_court = np.array([
+            (47, 0), (47, 50)
+        ], dtype=float)
 
-        # court_corners = np.array([
-        #     [0, 0], [94, 0], [94, 50], [0, 50]
-        # ], dtype=float)
-        # right_key = np.array([
-        #     (75, 19), (94, 19), (94, 31), (75, 31)
-        # ], dtype=float)
-        # half_court = np.array([
-        #     (47, 0), (47, 50)
-        # ], dtype=float)
-        #
-        # court_corners = court_corners.reshape(-1, 1, 2)  # need to reshape for transformation
-        # right_key = right_key.reshape(-1, 1, 2)
-        # half_court = half_court.reshape(-1, 1, 2)
-        #
-        # court_corners_video = cv2.perspectiveTransform(court_corners, H_court_to_video)
-        # right_key_video = cv2.perspectiveTransform(right_key, H_court_to_video)
-        # half_court_video = cv2.perspectiveTransform(half_court, H_court_to_video)
-        # # print(court_corners_video, court_corners_video.shape)
-        #
-        # court_corners_video = court_corners_video.astype(int).reshape(-1, 2)
-        # right_key_video = right_key_video.astype(int).reshape(-1, 2)
-        # half_court_video = half_court_video.astype(int).reshape(-1, 2)
-        #
-        # # prinkt(court_corners_video, court_corners_video.shape)
-        # pt1 = court_corners_video[0, :]
-        # pt2 = court_corners_video[1, :]
-        # pt3 = court_corners_video[2, :]
-        # pt4 = court_corners_video[3, :]
-        #
-        # cv2.line(draw_img, pt1, pt2, (0, 0, 255), 3)
-        # cv2.line(draw_img, pt2, pt3, (0, 0, 255), 3)
-        # cv2.line(draw_img, pt3, pt4, (0, 0, 255), 3)
-        # cv2.line(draw_img, pt4, pt1, (0, 0, 255), 3)
-        #
-        # pt1 = right_key_video[0, :]
-        # pt2 = right_key_video[1, :]
-        # pt3 = right_key_video[2, :]
-        # pt4 = right_key_video[3, :]
-        #
-        # cv2.line(draw_img, pt1, pt2, (0, 0, 255), 3)
-        # cv2.line(draw_img, pt2, pt3, (0, 0, 255), 3)
-        # cv2.line(draw_img, pt3, pt4, (0, 0, 255), 3)
-        # cv2.line(draw_img, pt4, pt1, (0, 0, 255), 3)
-        #
-        # pt1 = half_court_video[0, :]
-        # pt2 = half_court_video[1, :]
-        #
-        # cv2.line(draw_img, pt1, pt2, (0, 0, 255), 3)
+        court_corners = court_corners.reshape(-1, 1, 2)  # need to reshape for transformation
+        right_key = right_key.reshape(-1, 1, 2)
+        half_court = half_court.reshape(-1, 1, 2)
+
+        court_corners_video = cv2.perspectiveTransform(court_corners, H_court_to_video)
+        right_key_video = cv2.perspectiveTransform(right_key, H_court_to_video)
+        half_court_video = cv2.perspectiveTransform(half_court, H_court_to_video)
+        # print(court_corners_video, court_corners_video.shape)
+
+        court_corners_video = court_corners_video.astype(int).reshape(-1, 2)
+        right_key_video = right_key_video.astype(int).reshape(-1, 2)
+        half_court_video = half_court_video.astype(int).reshape(-1, 2)
+
+        # prinkt(court_corners_video, court_corners_video.shape)
+        pt1 = court_corners_video[0, :]
+        pt2 = court_corners_video[1, :]
+        pt3 = court_corners_video[2, :]
+        pt4 = court_corners_video[3, :]
+
+        cv2.line(draw_img, pt1, pt2, (0, 0, 255), 3)
+        cv2.line(draw_img, pt2, pt3, (0, 0, 255), 3)
+        cv2.line(draw_img, pt3, pt4, (0, 0, 255), 3)
+        cv2.line(draw_img, pt4, pt1, (0, 0, 255), 3)
+
+        pt1 = right_key_video[0, :]
+        pt2 = right_key_video[1, :]
+        pt3 = right_key_video[2, :]
+        pt4 = right_key_video[3, :]
+
+        cv2.line(draw_img, pt1, pt2, (0, 0, 255), 3)
+        cv2.line(draw_img, pt2, pt3, (0, 0, 255), 3)
+        cv2.line(draw_img, pt3, pt4, (0, 0, 255), 3)
+        cv2.line(draw_img, pt4, pt1, (0, 0, 255), 3)
+
+        pt1 = half_court_video[0, :]
+        pt2 = half_court_video[1, :]
+
+        cv2.line(draw_img, pt1, pt2, (0, 0, 255), 3)
 
         print(f'there are {len(src_pts)} src pts: {src_pts}')
 
